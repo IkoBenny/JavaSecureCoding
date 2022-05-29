fe450855efae975af43b41c7f088ebdd85135e41
Benny Iko <benny.iko.it@gmail.com>
2022-05-29 09:45:10 -0400

    On branch master  Initial commit Lessons Learned:
    Security
    SQL and other injection attacks
    
    Software Development
    URL encoding values in relation to spaces in pathname
    
    Maven
    Build Process
    Testing with JUnit
    
    Java
    Static block
    Secure coding practices
    Statements
    PreparedStatements
    Connecting to JDBC
    ClassLoader

---
+++ src/main/java/com/frankmoley/lil/security/data/PresidentDAO.java
@@ -0,0 +1,45 @@
+package com.frankmoley.lil.security.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.frankmoley.lil.security.util.DatabaseUtil;
+
+public class PresidentDAO {
+
+    public List<President> getByLastName(String lastName){
+        Connection connection = DatabaseUtil.getConnection();
+        String sql = "select * from PRESIDENT where LAST_NAME = ?";
+        List<President> resultList= new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1,lastName);
+            ResultSet resultSet = statement.executeQuery();
+            while(resultSet.next()){
+                resultList.add(processResultSet(resultSet));
+            }
+            resultSet.close();
+            statement.close();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return resultList;
+    }
+
+
+    private President processResultSet(ResultSet resultSet) throws SQLException{
+        Long id = resultSet.getLong("PRESIDENT_ID");
+        String firstName = resultSet.getString("FIRST_NAME");
+        String middleInitial = resultSet.getString("MIDDLE_INITIAL");
+        String lastName = resultSet.getString("LAST_NAME");
+        String emailAddress = resultSet.getString("EMAIL_ADDRESS");
+        return new President(id, firstName, middleInitial, lastName, emailAddress);
+    }
+
+
+}
